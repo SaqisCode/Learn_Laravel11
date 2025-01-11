e9b225455c3f3b85d198f5e4f9271f732ae726b8
SaqisCode <satrianuqitaufani@gmail.com>
2025-01-11 15:12:42 +0700

    test

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman BLog</title>
+</head>
+<body>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam consequatur nihil fugit nisi a aspernatur minus nobis id eaque. Molestiae pariatur iure voluptatibus sed, amet aspernatur nulla temporibus architecto voluptatem.</p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam consequatur nihil fugit nisi a aspernatur minus nobis id eaque. Molestiae pariatur iure voluptatibus sed, amet aspernatur nulla temporibus architecto voluptatem.</p>
+    </article>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home', ['nama' => 'Satria76']);
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
